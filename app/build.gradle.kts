3917b654c4332e4b69a660ff0fd963d63182c653
varuntej411 <derangulavaruntej@gmail.com>
2024-07-07 00:15:36 +0530

    All dependencies Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+    id("kotlin-android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -59,8 +62,57 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //navigation compose
+    implementation(libs.androidx.navigation.compose)
+    //hilt dagger
+    implementation(libs.androidx.hilt.viewmodel)
+    //  kapt(libs.androidx.hilt.compiler)
+    kapt(libs.google.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.hilt)
+
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+
+    //Room Database
+    //noinspection KaptUsageInsteadOfKsp
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.guava)
+    // implementation(libs.androidx.room.coroutines)
+    implementation(libs.androidx.room.common)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.paging.guava)
+
+    // OkHttp3
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(platform(libs.okhttp.bom))
+
+    //coil image loader
+    implementation(libs.androidx.coil.compose)
+    //Glide image loader
+    implementation(libs.androidx.glider)
+    //noinspection KaptUsageInsteadOfKsp
+    kapt(libs.androidx.glider.compiler)
+
+    //retrofit
+    implementation(libs.retrofit)
+    implementation(libs.google.gson)
+    implementation(libs.converter.gson)
+    implementation(libs.converter.jackson)
+    implementation(libs.converter.moshi)
+    implementation(libs.converter.scalars)
+    implementation(libs.retrofit.converters)
+    implementation(libs.retrofit.adapters)
+
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
+    testImplementation(libs.mockwebserver)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin.serialization) apply false
+    // alias(libs.plugins.ksp.version) apply false
+    alias(libs.plugins.hilt.version) apply false
+    alias(libs.plugins.room.version) apply false
 }
